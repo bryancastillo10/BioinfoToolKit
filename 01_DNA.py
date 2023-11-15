dad97855c1cb2e4a6b13b4900f95023c424393c9
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2023-11-15 22:16:28 +0800

    initial commit

---
+++ 01_DNA.py
@@ -0,0 +1,7 @@
+from DNATools import *
+import random
+
+rDNAstr = ''.join([random.choice(Nucleotide)
+        for nuc in range(50)])
+
+print(validateSeq(rDNAstr))
---
+++ DNATools.py
@@ -0,0 +1,8 @@
+Nucleotide = ["G","T","A","C"]
+
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotide:
+            return False
+    return tmpseq
