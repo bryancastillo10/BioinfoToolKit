f5790a4607018eb0118ed7e529ed6d604d7bad16
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2023-11-16 22:07:24 +0800

    DNA Toolkit Complete

--- DNAToolkits.py
+++ DNAToolkits.py
@@ -1,5 +1,8 @@
-Nucleotide = ["G","T","A","C"]
 import collections
+
+Nucleotide = ["G","T","A","C"]
+DNA_ReverseComplement = {'T':'A','A':'T','G':'C','C':'G'}
+
 
 def validateSeq(dna_seq):
     tmpseq = dna_seq.upper()
@@ -9,8 +12,15 @@ def validateSeq(dna_seq):
     return tmpseq
 
 def countNucFrequency(seq):
-    # tmpFreqDict = {"A":0 ,"C":0,"T":0,"G":0}
-    # for nuc in seq:
-    #     tmpFreqDict[nuc] += 1
-    # return tmpFreqDict
-    return dict(collections.Counter(seq))
+    tmpFreqDict = {"A":0 ,"C":0,"T":0,"G":0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+    # return dict(collections.Counter(seq))
+
+def transcription(seq):
+    # DNA -> RNA Transcription
+    return seq.replace("T","U")
+
+def reverse_complement(seq):
+    return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from DNAToolkits import *
+from utilities.colored import color
 import random
 
-rDNAstr = ''.join([random.choice(Nucleotide)
-        for nuc in range(50)])
+randDNAstr = ''.join([random.choice(Nucleotide)
+        for nuc in range(60)])
 
-print(validateSeq(rDNAstr))
-print(countNucFrequency(rDNAstr))
+DNAStr = validateSeq(randDNAstr)
+print(f'\n Sequence: {color(DNAStr)}\n')
+print(f'[1]  Sequence Length: {len(DNAStr)}\n')
+print(color(f'[2]  Nucleotide Frequency: {countNucFrequency(DNAStr)})\n'))
+print(f'[3]  DNA to RNA Transcription: {color(transcription(DNAStr))}\n')
+
+print(f"[4]  DNA String + Reverse Complement: \n5' {color(DNAStr)} 3'")
+print(f"   {''.join(['|' for i in range(len(DNAStr))])}")
+print(f"3' {color(reverse_complement(DNAStr))} 5'\n")
